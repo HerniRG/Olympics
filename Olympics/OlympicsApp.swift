689bc571a52c57e9b3c8a7b86cda9da6b0be18b8
hernirg <hernanrg85@gmail.com>
2024-08-08 17:04:58 +0200

    Initial Commit

---
+++ Olympics/OlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympicsApp.swift
+//  Olympics
+//
+//  Created by Hernán Rodríguez on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
